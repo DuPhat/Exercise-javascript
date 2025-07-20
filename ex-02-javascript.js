7d4a921507e21725b0751f347f1a1fdd26a0fc78
DU PHAT <duphat2003@gmail.com>
2025-07-20 23:56:39 +0700

    Update code

---
+++ ex-02-javascript.js
@@ -0,0 +1,12 @@
+var use = {
+    HoVaTen:"Du Tan Phat",
+    NamSinh:"2003",
+    banBe:['Như Ngọc', 'Sơn Phát','Nhựt Minh'],
+    diaChi:{
+        Tinh: 'HoChiMinh', Quan: 'Q.TanBinh'
+    },
+};
+console.log(use.HoVaTen, use.NamSinh, use.banBe, use.diaChi);
+
+
+    
